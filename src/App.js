c98bded95d5c52b45c857c15ee3ddfaeebebccb8
JieHe <jiehe1114@gmail.com>
2018-02-25 22:54:54 +1100

    Test using Jest and Enzyme

--- src/App.js
+++ src/App.js
@@ -12,30 +12,35 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.results = mockData.results;
-        this.saved = mockData.saved;
         this.state = {
             showAddButton: 'none',
             showRemoveButton: 'none',
+            saved : mockData.saved
         }
     }
 
     removeProperty(id) {
-        /*this.results.push(this.saved.find(item => item.id === id));*/
-        this.saved = this.saved.filter(function(item) {
-            return item.id !== id;
-        });
-        
+        const saved = this.state.saved.filter(s => s.id !== id);
+        this.setState({ saved });
+    }
+
+    addProperty(id){
+        if(!this.state.saved.find(item => item.id === id)){
+            const newSaved = this.state.saved.slice();
+            newSaved.push(this.results.find(item => item.id === id));
+            this.setState({ saved : newSaved });
+        }
     }
 
     getButton(id){
-        if(this.saved.find(item => item.id === id)){
-            return (<Button disabled>Saved</Button>);
-        }else if(this.state.showAddButton === id){
-            return (
-                <Button onClick={() => 
-                    this.saved.push(
-                        this.results.find(item => item.id === id))}>Add Property
-                </Button>);
+
+        if(this.state.showAddButton === id){
+            const savedList = this.state.saved;
+            if(savedList.find(item => item.id === id)){
+                return (<Button className="disable" disabled>Saved</Button>);
+            }else{
+                return (<Button onClick={() => this.addProperty(id)}>Add Property</Button>);
+            }
         }else{
             return null;
         }
@@ -43,56 +48,46 @@ class App extends Component {
 
     render() {
         return (
-            <div >
-                
+            <div >   
                 <h1 className="title"><img src={reaLogo} className="rea-logo"/> CODE CHALLENGE</h1>
                 <Row className="app">
-                    <Col xl="4" lg="4" md="12" className="result">
+                    <Col xl="4" lg="4" md="12" className="result" id='results'>
                         <h3>Results</h3>
                         <div className=" dashed-box">
-
-                            {
-                                this.results.map((data,index) => {
-                                    return(
-                                        <div className="box" onMouseOver={() => this.setState({showAddButton: data.id})}>
-                                            <ResultList key={index} data={data}/>
-                                            <div className="btn-style">
-                                                {this.getButton(data.id)}
-                                            </div>
+                            {this.results.map((data,index) => {
+                                return(
+                                    <div className="box" id={"box" + data.id} 
+                                        onMouseOver={() => this.setState({showAddButton: data.id})}
+                                        onMouseLeave = {() => this.setState({showAddButton: null})}>
+                                        <ResultList key={index} data={data}/>
+                                        <div className="btn-style">
+                                            {this.getButton(data.id)}
                                         </div>
-                                    )
-                                }) 
-                            }
-
+                                    </div>)
+                            })}
                         </div>
-
                     </Col>
 
-                    <Col xl="4" lg="4" md="12" className="save">
+                    <Col xl="4" lg="4" md="12" className="save" id="saved">
                         <h3>Saved Properties</h3>
                         <div className=" dashed-box">
-
-                            {
-                                this.saved.map((data,index) => {
-                                    return(
-                                        <div className="box" onMouseOver={() => this.setState({showRemoveButton: data.id})}>
+                            {this.state.saved.map((data,index) => {
+                                return(
+                                        <div className="box" id={"box" + data.id} 
+                                        onMouseOver={() => this.setState({showRemoveButton: data.id})}
+                                        onMouseLeave = {() => this.setState({showRemoveButton: null})}>
                                             <SavedPropList key={index} data={data}/>
                                             <div className="btn-style">
-                                                {this.state.showRemoveButton === data.id ? (<Button onClick={() => this.removeProperty(data.id)}>Remove Property</Button>) : null}
+                                                {this.state.showRemoveButton === data.id ? 
+                                                    (<Button onClick={
+                                                        () => this.removeProperty(data.id)}>Remove Property
+                                                    </Button>) : null}
                                             </div>
-
-
-                                        </div>
-
-                                    )
-                                })
-                            }
-
+                                        </div>)
+                            })}
                         </div>
-
                     </Col>               
                 </Row>
-
             </div>
         );
     }
--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,15 +1,160 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-dom/test-utils';
 import { shallow } from 'enzyme';
+import mockData from '../mockdata.json';
 import App from '../App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+describe('Component Count: App page', () => {
+	it('renders without crashing', () => {
+	  const div = document.createElement('div');
+	  ReactDOM.render(<App />, div);
+	  ReactDOM.unmountComponentAtNode(div);
+	});
+
+	it('renders 1 <App /> component', () => {
+		const appPage = shallow(<App/>);
+		expect(appPage.length).toEqual(1);
+	});
+
+	it('renders 1 <result /> component', () => {
+		const wrapper = shallow(<App/>);
+		const result = wrapper.find('#results');
+		// console.log("result:" + result.length);
+		expect(result.length).toEqual(1);
+	});
+
+	it('renders 1 <saved /> component', () => {
+		const wrapper = shallow(<App/>);
+		const saved = wrapper.find('#saved');
+		// console.log("saved:" + saved.length);
+		expect(saved.length).toEqual(1);
+	});
 });
 
-it('renders 1 <App /> component', () => {
-	const appPage = shallow(<App/>);
-	expect(appPage.length).toEqual(1);
+describe('Mouse over events: App page', () => {
+	it('Default show 0 button', () => {
+		const wrapper = shallow(<App/>);
+		// console.log(wrapper.find('button').length);
+		expect(wrapper.find('button').length).toEqual(0);
+	});
+
+	// ReactTestUtils.Simulate.mouseOver not available!
+	it('Mouse over show button in result', () => {
+		const wrapper = shallow(<App/>);
+		const mockId = mockData["results"][0].id;
+
+		// skip mouse over event
+		wrapper.setState({showAddButton : mockId});
+		expect(wrapper.find('Button').length).toEqual(1);
+
+		const domId = '#box' + mockId;
+		const box = wrapper.find(domId);
+		expect(box.find('Button').length).toEqual(1);
+	});
+
+	// ReactTestUtils.Simulate.mouseOver not available!
+	it('Mouse over show button in saved', () => {
+		const wrapper = shallow(<App/>);
+		const mockId = mockData["saved"][0].id;
+
+		// skip mouse over event
+		wrapper.setState({showRemoveButton : mockId});
+		expect(wrapper.find('Button').length).toEqual(1);
+
+		const domId = '#box' + mockId;
+		const box = wrapper.find(domId);
+		expect(box.find('Button').length).toEqual(1);
+	});
 });
+
+
+describe('Mouse click events: add property', () => {
+	it('Mouse click add property button in result, saved +1', () => {
+		const wrapper = shallow(<App/>);
+		const boxesNum0 = wrapper.find(".save").find(".box").length;
+
+		const mockId = mockData["results"][0].id;
+		wrapper.setState({showAddButton : mockId});
+
+		const btn = wrapper.find("Button").at(0);
+		btn.simulate('click');
+		const boxesNum1 = wrapper.find(".save").find(".box").length;
+		expect(boxesNum1).toEqual(boxesNum0 + 1);
+	});
+
+	it('Try Mouse click twice on add property button in result, saved stay same', () => {
+		const wrapper = shallow(<App/>);
+		const boxesNum0 = wrapper.find(".save").find(".box").length;
+
+		const mockId = mockData["results"][0].id;
+		wrapper.setState({showAddButton : mockId});
+
+		const btn = wrapper.find("Button").at(0);
+		btn.simulate('click');
+		const boxesNum1 = wrapper.find(".save").find(".box").length;
+
+		btn.simulate('click');
+		const boxesNum2 = wrapper.find(".save").find(".box").length;
+		expect(boxesNum1).toEqual(boxesNum2);
+	});
+
+	it('Mouse click on add property, button change to disable', () => {
+		const wrapper = shallow(<App/>);
+		const mockId = mockData["results"][0].id;
+		wrapper.setState({showAddButton : mockId});
+
+		const btn = wrapper.find("Button").at(0);
+
+		btn.simulate('click');
+		const btnAddNew = wrapper.find("Button").at(0);
+		expect(btnAddNew.find(".disable").length).toEqual(1);
+	});
+});
+
+
+describe('Mouse click events: remove property', () => {
+
+	it('Mouse click remove property button, saved elements -1', () => {
+		const wrapper = shallow(<App/>);
+		const mockId = mockData["saved"][0].id;
+		wrapper.setState({showRemoveButton : mockId});
+
+		const btn = wrapper.find("Button").at(0);
+		const boxesNum0 = wrapper.find(".save").find(".box").length;
+		// expect(wrapper.find(".disabled").length).toEqual(0);
+
+		btn.simulate('click');
+		const boxesNum1 = wrapper.find(".save").find(".box").length;
+		expect(boxesNum0).toEqual(boxesNum1 + 1);
+	});
+
+	it('Mouse click remove property button, update the saved btn in result list', () => {
+		const wrapper = shallow(<App/>);
+		const mockId = mockData["results"][0].id;
+		wrapper.setState({showAddButton : mockId});
+		expect(wrapper.find(".disable").length).toEqual(0);
+
+		// Click on the add button in Result list
+		const btnAdd = wrapper.find(".result").find("Button").at(0);
+		btnAdd.simulate('click');
+		expect(wrapper.find(".disable").length).toEqual(1);
+
+		// Click on the remove btn in saved list
+		wrapper.setState({showRemoveButton : mockId});
+		const btnRemove = wrapper.find(".save").find("Button").at(0);
+		btnRemove.simulate('click');
+
+		// Check the disable btn number
+		expect(wrapper.find(".disable").length).toEqual(0);
+	});
+});
+
+
+
+
+
+
+
+
